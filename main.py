ccf7ea7659d06b7a41703f0f27b94f8270099013
Sudarshan333u <67819655+Sudarshan333u@users.noreply.github.com>
2021-02-01 13:59:39 +0530

    testing

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def token_required(f):
          return jsonify({'message': 'a valid token is missing'})
 
       print(token)
+      print('hii')
       # if not firebase_admin._apps:
       #   default_app = initialize_app(cred)
       # db = firestore.client()
